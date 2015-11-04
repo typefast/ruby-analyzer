31038dd96146d4530623e24075a8a555625a38c1
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-04 18:32:44 +0000

    Excluding whitespace

--- analyzer.rb
+++ analyzer.rb
@@ -3,6 +3,8 @@ lines = File.readlines("text.txt")
 line_count = lines.size
 text = lines.join
 total_characters = text.length
+excluding_whitespace = text.gsub(/\s+/, '').length
 
 puts "#{line_count} lines"
 puts "#{total_characters} characters"
+puts "#{excluding_whitespace} (excluding spaces)"
