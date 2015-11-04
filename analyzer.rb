beac3ede1fa5445220550378e36edc93d2913a41
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-04 18:40:13 +0000

    Word Count

--- analyzer.rb
+++ analyzer.rb
@@ -4,7 +4,9 @@ line_count = lines.size
 text = lines.join
 total_characters = text.length
 excluding_whitespace = text.gsub(/\s+/, '').length
+word_count = text.split.length
 
 puts "#{line_count} lines"
 puts "#{total_characters} characters"
 puts "#{excluding_whitespace} (excluding spaces)"
+puts "#{word_count} words"
