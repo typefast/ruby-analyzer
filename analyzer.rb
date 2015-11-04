3b195b36b716fb898a53707ba128042b5ac4a4c8
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-04 18:46:06 +0000

    sentance count And paragraph count

--- analyzer.rb
+++ analyzer.rb
@@ -5,8 +5,12 @@ text = lines.join
 total_characters = text.length
 excluding_whitespace = text.gsub(/\s+/, '').length
 word_count = text.split.length
+sentance_count = text.split(/\.|\?|!/).length
+paragraph_count = text.split(/\n\n/).length
 
 puts "#{line_count} lines"
 puts "#{total_characters} characters"
 puts "#{excluding_whitespace} (excluding spaces)"
 puts "#{word_count} words"
+puts "#{sentance_count} sentances"
+puts "#{paragraph_count} paragraphs"
